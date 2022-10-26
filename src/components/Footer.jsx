c794ca9a7273028e7589e8b8946b764ca45c445e
anisa2472 <anisa.psari24@gmail.com>
2022-10-26 20:56:02 +0700

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="bg-dark w-full p-6 text-center text-gray-50">
-            @ 2022 Anisa Permatasari
+            @ 2022 <span className="text-orange-2">Anisa Permatasari</span>
         </div>
     )
 }
